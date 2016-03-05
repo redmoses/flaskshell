d82c0b2914d094101d8e463b0fb3eba62568cfe8
Redmoses <musa.dhk@gmail.com>
2016-03-06 02:25:24 +0600

    fixed query format and result presentation

--- src/app.py
+++ src/app.py
@@ -10,9 +10,9 @@ db_host = 'mysql'
 # whitelist
 ip_whitelist = []
 # queries
-query_success = "SELECT COUNT(*) FROM flaskshell.tasks WHERE task_status='Success'"
-query_pending = "SELECT COUNT(*) FROM flaskshell.tasks WHERE task_status='Pending'"
-query_failed = "SELECT COUNT(*) FROM flaskshell.tasks WHERE task_status='Failed'"
+query_success = "SELECT COUNT(*) FROM flaskshell.tasks WHERE task_status=\"Success\";"
+query_pending = "SELECT COUNT(*) FROM flaskshell.tasks WHERE task_status=\"Pending\";"
+query_failed = "SELECT COUNT(*) FROM flaskshell.tasks WHERE task_status=\"Failed\";"
 
 
 def valid_ip():
@@ -28,25 +28,33 @@ def valid_ip():
 
 @app.route('/status/')
 def get_status():
+    result = """
+    <title>Status Page</title>
+    <h1>Your status</h1>
+    <ul>
+        <li>Success: {0}</li>
+        <li>Pending: {1}</li>
+        <li>Failure: {2}</li>
+    </ul>
+    """
     if valid_ip():
-        base_command = "mysql -u{0} -p{1} -h ${2}".format(
+        base_command = "mysql -u{0} -p{1} -h {2}".format(
             db_user, db_pass, db_host
         )
         command_success = "{0} -e '{1}'".format(base_command, query_success)
         command_pending = "{0} -e '{1}'".format(base_command, query_pending)
         command_failed = "{0} -e '{1}'".format(base_command, query_failed)
-
         try:
             result_success = subprocess.check_output(
-                [command_success], shell=True)
+                [command_success], shell=True).decode('ascii').replace('COUNT(*)','')
             result_pending = subprocess.check_output(
-                [command_pending], shell=True)
+                [command_pending], shell=True).decode('ascii').replace('COUNT(*)','')
             result_failed = subprocess.check_output(
-                [command_failed], shell=True)
+                [command_failed], shell=True).decode('ascii').replace('COUNT(*)','')
         except subprocess.CalledProcessError as e:
-            return "An error occurred while trying to fetch task status updates."
+            return "An error occurred while trying to fetch task status updates. %s" % command_success
 
-        return 'Success %s, Pending %s, Failed %s' % (result_success, result_pending, result_failed)
+        return result.format(result_success, result_pending, result_failed)
     else:
         return """
         <title>404 Not Found</title>
